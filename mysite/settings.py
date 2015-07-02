bb5da0393e24312c5c0e6740edafcf18763ac92e
Charles Huang <Charles.Huang@iress.com.au>
2015-07-02 16:57:00 +1000

    favicon fixup

--- mysite/settings.py
+++ mysite/settings.py
@@ -61,7 +61,9 @@ ROOT_URLCONF = 'mysite.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR, 'templates'),
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -114,6 +116,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # files_widget
 MEDIA_URL = '/media/'
